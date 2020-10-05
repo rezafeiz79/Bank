41b2ef1e180a37d2cae9ced0420bc490298ae63f
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-05 14:11:43 +0330

    Added Fields, Constructor, Getters to Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,29 @@
 public abstract class Person {
+    private String name;
+    private String nationalCode;
+    private String userName;
+    private String password;
+
+    public Person(String name, String nationalCode, String userName, String password) {
+        this.name = name;
+        this.nationalCode = nationalCode;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
