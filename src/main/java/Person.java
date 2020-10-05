377ed4e67c4c606d97159e5524dca4ee38f1bf8e
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-05 14:40:56 +0330

    Added Hibernate Annotations For Person Class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,14 +1,27 @@
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class Person {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String name;
     private String nationalCode;
     private String userName;
     private String password;
 
-    public Person(String name, String nationalCode, String userName, String password) {
+    public Person(Integer id, String name, String nationalCode, String userName, String password) {
+        this.id = id;
         this.name = name;
         this.nationalCode = nationalCode;
         this.userName = userName;
         this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getName() {
