a1bd6a0077503e5dcd09cec8448a98941ceda465
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-12 18:00:50 +0330

    added annotation and fields and constructor and getters to transaction class

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,2 +1,41 @@
+import javax.persistence.*;
+
+@Entity
 public class Transaction {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @OneToOne(cascade = CascadeType.ALL)
+    private CreditCard senderCard;
+    @OneToOne(cascade = CascadeType.ALL)
+    private CreditCard receiverCard;
+    private Long amount;
+    private String date;
+
+    public Transaction(Integer id, CreditCard senderCard, CreditCard receiverCard, Long amount, String date) {
+        this.id = id;
+        this.senderCard = senderCard;
+        this.receiverCard = receiverCard;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public CreditCard getSenderCard() {
+        return senderCard;
+    }
+
+    public CreditCard getReceiverCard() {
+        return receiverCard;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
