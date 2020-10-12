aa75e1ade397a3e572c8f5a565369dd7a3e55fcd
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-12 17:33:55 +0330

    added annotation and fields and constructor and getters to branchManager class

--- src/main/java/BranchManager.java
+++ src/main/java/BranchManager.java
@@ -1,2 +1,27 @@
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.Set;
+
+@Entity
 public class BranchManager extends Person {
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Employee> employees;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Branch branch;
+
+    public BranchManager(Integer id, String name, String nationalCode, String userName, String password, Set<Employee> employees, Branch branch) {
+        super(id, name, nationalCode, userName, password);
+        this.employees = employees;
+        this.branch = branch;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
 }
