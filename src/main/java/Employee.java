1f8f198daecea0e3fdab4a41ddd6110efd8dfa52
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-12 17:30:22 +0330

    added annotation and fields and constructor and getters to employee class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,25 @@
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Employee extends Person {
+    @OneToOne(cascade = CascadeType.ALL)
+    private BranchManager branchManager;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Branch branch;
+
+    public Employee(Integer id, String name, String nationalCode, String userName, String password, BranchManager branchManager, Branch branch) {
+        super(id, name, nationalCode, userName, password);
+        this.branchManager = branchManager;
+        this.branch = branch;
+    }
+
+    public BranchManager getBranchManager() {
+        return branchManager;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
 }
