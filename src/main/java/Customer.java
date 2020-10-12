61d910852b3f2a73d5fca4c1ffd291450a34bab5
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-12 17:26:14 +0330

    Added Annotation And accounts filed and constructor to customer class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,19 @@
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
 public class Customer extends Person {
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Account> accounts;
+
+    public Customer(Integer id, String name, String nationalCode, String userName, String password, Set<Account> accounts) {
+        super(id, name, nationalCode, userName, password);
+        this.accounts = accounts;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
 }
