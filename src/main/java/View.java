703618dc767f285a9d48bec4a9e9d23a1879698d
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-13 18:46:28 +0330

    edited view class

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class View {
     private static Scanner scanner = new Scanner(System.in);
@@ -18,7 +19,7 @@ public class View {
         System.out.println("1. Branch CRUD");
         System.out.println("2. BranchManager CRUD");
         System.out.println("3. Employee CRUD");
-        System.out.println("4. Transaction CRUD");
+        System.out.println("4. Transaction CRD");
         System.out.println("5. Account CRUD");
         System.out.println("6. Customer CRUD");
         System.out.println("7. CreditCards CRUD");
@@ -66,5 +67,79 @@ public class View {
         System.out.print("Enter The Number Of Your Choice: ");
         choice = Integer.parseInt(scanner.nextLine());
         return choice;
+    }
+
+    public static int showTransactionCRDMenu() {
+        int choice;
+        System.out.println("1. Create Transaction");
+        System.out.println("2. Get List Of Transactions");
+        System.out.println("3. Get Transaction By ID");
+        System.out.println("4. Delete Employee");
+        System.out.println("5. Back To Previous Menu");
+        System.out.print("Enter The Number Of Your Choice: ");
+        choice = Integer.parseInt(scanner.nextLine());
+        return choice;
+    }
+
+    public static int showAccountCRUDMenu() {
+        int choice;
+        System.out.println("1. Create Account");
+        System.out.println("2. Get List Of Accounts");
+        System.out.println("3. Get Account By ID");
+        System.out.println("4. Update Account");
+        System.out.println("5. Delete Account");
+        System.out.println("6. Back To Previous Menu");
+        System.out.print("Enter The Number Of Your Choice: ");
+        choice = Integer.parseInt(scanner.nextLine());
+        return choice;
+    }
+
+    public static int showCustomerCRUDMenu() {
+        int choice;
+        System.out.println("1. Create Customer");
+        System.out.println("2. Get List Of Customers");
+        System.out.println("3. Get Customer By ID");
+        System.out.println("4. Update Customer");
+        System.out.println("5. Delete Customer");
+        System.out.println("6. Back To Previous Menu");
+        System.out.print("Enter The Number Of Your Choice: ");
+        choice = Integer.parseInt(scanner.nextLine());
+        return choice;
+    }
+
+    public static int showCreditCardCRUDMenu() {
+        int choice;
+        System.out.println("1. Create CreditCard");
+        System.out.println("2. Get List Of CreditCards");
+        System.out.println("3. Get CreditCard By ID");
+        System.out.println("4. Update CreditCard");
+        System.out.println("5. Delete CreditCard");
+        System.out.println("6. Back To Previous Menu");
+        System.out.print("Enter The Number Of Your Choice: ");
+        choice = Integer.parseInt(scanner.nextLine());
+        return choice;
+    }
+
+    public static int showReportsMenu() {
+        int choice;
+        System.out.println("1. Get Total Balance Of Bank");
+        System.out.println("2. Back To Previous Menu");
+        System.out.print("Enter The Number Of Your Choice: ");
+        choice = Integer.parseInt(scanner.nextLine());
+        return choice;
+    }
+
+    public static Branch showBranchCreationWizard() {
+        return null;
+    }
+
+    public static <T> void showListOfEntities(Set<T> set) {
+        for (T element : set) {
+            System.out.println(element.toString());
+        }
+    }
+
+    public static <T> void showEntity(T element) {
+        System.out.println(element.toString());
     }
 }
