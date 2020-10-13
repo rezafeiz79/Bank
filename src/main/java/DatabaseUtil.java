f58c48dba8a9d2f94a625c83a2332ab32e26b87d
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-13 19:38:46 +0330

    edited databaseUtil class

--- src/main/java/DatabaseUtil.java
+++ src/main/java/DatabaseUtil.java
@@ -1,5 +1,9 @@
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class DatabaseUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
@@ -16,6 +20,24 @@ public class DatabaseUtil {
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
+    }
+
+    public static <T> Set<T> getAllEntities(Session session, Class<T> type) {
+        Set<T> set = new HashSet<T>();
+        set.addAll(session.createQuery("SELECT a FROM " + type.getSimpleName() + " a", type).getResultList());
+        return set;
+    }
+
+    public static <T> void saveEntity(Session session, T object) {
+        session.save(object);
+    }
+
+    public static <T> void deleteEntity(Session session, T object) {
+        session.delete(object);
+    }
+
+    public static <T> void updateEntity(Session session, T object) {
+        session.update(object);
     }
 
     public static void shutdown() {
