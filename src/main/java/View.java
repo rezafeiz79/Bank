112307027cdc77c3b0446d9b6db17510ccac196d
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-13 16:04:27 +0330

    edited view class

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class View {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int showMainMenu() {
+        int choice;
+        System.out.println("1. Sign In");
+        System.out.println("2. Sign Up As Customer");
+        System.out.println("3. Exit");
+        System.out.print("Enter The Number Of Your Choice: ");
+        choice = Integer.parseInt(scanner.nextLine());
+        return choice;
+    }
 }
