d7246f08ee948bfb4f8b3dcbdbb5c0227b685211
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-13 16:06:30 +0330

    added annotation and constructor mainManager class

--- src/main/java/MainManager.java
+++ src/main/java/MainManager.java
@@ -1,2 +1,8 @@
-public class MainManager {
+import javax.persistence.Entity;
+
+@Entity
+public class MainManager extends Person {
+    public MainManager(Integer id, String name, String nationalCode, String userName, String password) {
+        super(id, name, nationalCode, userName, password);
+    }
 }
