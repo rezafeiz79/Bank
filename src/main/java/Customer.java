38457ae7e40d41bcb5bd48c9bc72f688fc57f92e
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-15 18:09:25 +0330

    added constructor and setters to customer class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -13,9 +13,17 @@ public class Customer extends Person {
         this.accounts = accounts;
     }
 
+    public Customer(Integer id, String name, String nationalCode, String userName, String password) {
+        super(id, name, nationalCode, userName, password);
+    }
+
     public Customer() { }
 
     public Set<Account> getAccounts() {
         return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
     }
 }
