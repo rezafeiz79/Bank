4d17dfa1a83304799b99b331a0e40e88b59976d9
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-16 12:13:59 +0330

    added mainManagerMenu enum

---
+++ src/main/java/MenuOptions/MainMangerMenu.java
@@ -0,0 +1,13 @@
+package MenuOptions;
+
+public enum MainMangerMenu {
+    BRANCH_CRUD,
+    BRANCH_MANAGER_CRUD,
+    EMPLOYEE_CRUD,
+    TRANSACTION_CRD,
+    ACCOUNT_CRUD,
+    CUSTOMER_CRUD,
+    CREDIT_CARD_CRUD,
+    REPORTS,
+    BACK
+}
