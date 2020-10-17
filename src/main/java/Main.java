049f426c91f6871df2e82efae192db500f71c2c3
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-17 21:59:42 +0330

    edited main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,18 @@ import org.hibernate.Session;
 public class Main {
     public static void main(String[] args) {
         Session session = DatabaseUtil.getSessionFactory().openSession();
+        MenuOptions.MainMenu mainMenuOption;
+        boolean mainFlag = true;
+        while(mainFlag) {
+            mainMenuOption = View.showMainMenu();
+            switch (mainMenuOption) {
+                case SIGNIN:
+                    break;
+                case SIGNUP:
+                    break;
+                case EXIT:
+                    mainFlag = false;
+            }
+        }
     }
 }
