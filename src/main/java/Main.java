a5fe48bcd39f230c622da04ad0b91a8dd9d1f122
rezafeiz79 <rezafeiz79@gmail.com>
2020-10-17 22:08:34 +0330

    edited main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import org.hibernate.Session;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         Session session = DatabaseUtil.getSessionFactory().openSession();
@@ -9,6 +12,17 @@ public class Main {
             mainMenuOption = View.showMainMenu();
             switch (mainMenuOption) {
                 case SIGNIN:
+                    Set<Person> persons = new HashSet<>();
+                    persons.addAll(DatabaseUtil.getAllEntities(session, MainManager.class));
+                    persons.addAll(DatabaseUtil.getAllEntities(session, BranchManager.class));
+                    persons.addAll(DatabaseUtil.getAllEntities(session, Employee.class));
+                    persons.addAll(DatabaseUtil.getAllEntities(session, Customer.class));
+                    Person person = View.showSignInWizard(persons);
+                    if (person != null) {
+                        if (person instanceof MainManager) {
+                            View.showMainManagerMenu();
+                        }
+                    }
                     break;
                 case SIGNUP:
                     break;
